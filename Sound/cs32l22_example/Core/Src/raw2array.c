de7f65d34dd23b81a06653a34e38a95bc24a9e72
kobyakov <dmi.kobyakov@gmail.com>
2022-02-13 02:33:43 +0300

    Function for creating plots

--- Sound/cs32l22_example/Core/Src/raw2array.c
+++ Sound/cs32l22_example/Core/Src/raw2array.c
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include <arpa/inet.h>
 
+//const size_t maxSize = 250000;
 const size_t maxSize = 500000;
-
 
 
 int main(int argc, char** argv)
@@ -63,5 +63,44 @@ int main(int argc, char** argv)
     fclose(rawAudioFile);
     fclose(cArrayFile);
 
+    exit(EXIT_SUCCESS);
+}
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+// gnuplot -> plot 'sample.dat'
+
+extern const size_t audioSampleLength;
+extern const uint8_t audioSample[];
+
+static inline float pcm16BitToFloaT(int16_t p)
+{
+    float f = (float)p / (float)32768;
+    if (f > 1) f = 1;
+    if (f < -1) f = -1;
+    return f;
+}
+
+int main()
+{
+    FILE *cArrayFile;
+    if ((cArrayFile = fopen("sample.dat", "w")) == NULL) {
+        perror("can't open c array file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int16_t inputData[2];
+
+    for (size_t i = 0; i < audioSampleLength; i+=2) {
+        inputData[0] = (int16_t)(audioSample[i] << 8 | 0x00);
+        inputData[1] = (int16_t)(audioSample[i+1] << 8 | 0x00);
+        fprintf(cArrayFile, "%.16f\n", pcm16BitToFloaT(inputData[0]));
+    }
+
+    fclose(cArrayFile);
     exit(EXIT_SUCCESS);
 }
